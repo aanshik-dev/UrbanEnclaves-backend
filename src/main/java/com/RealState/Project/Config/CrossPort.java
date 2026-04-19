df18b01377f50e6a8a54b837660c778427cf3c97
aanshik-dev <aanshiksinghtomar@gmail.com>
2026-04-19 19:39:17 +0530

    Updated the Listing accept

--- src/main/java/com/RealState/Project/Config/CrossPort.java
+++ src/main/java/com/RealState/Project/Config/CrossPort.java
@@ -11,7 +11,7 @@ public class CrossPort implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
                 .allowedOrigins("http://localhost:5173") // Your React URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
